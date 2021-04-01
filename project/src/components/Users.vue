01a49cc75bbe3b374bd360c852b4c49f3a9c88af
melissateozuki <Melissateozuki@gmail.com>
2021-04-01 10:48:12 +0800

    Add usernames into firebase as family collection

--- project/src/components/Users.vue
+++ project/src/components/Users.vue
@@ -1,80 +1,96 @@
-
-Search Vuetify (press /)
-Premiere Sponsors
-#List item groups
-The v-list-item-group provides the ability to create a group of selectable v-list-items. The v-list-item-group component utilizes v-item-group at its core to provide a clean interface for interactive lists.
-
-
-#Usage
-By default, the v-list-item-group operates similarly to v-item-group. If a value is not provided, the group will provide a default based upon its index.
-
-#API
-v-list-group
-v-list-item
-v-list-item-action
-v-list-item-action-text
-v-list-item-avatar
-v-list-item-content
-v-list-item-group
-v-list-item-subtitle
-v-list-item-title
-#Examples
-#Props
-#Active class
-You can set a class which will be added when an item is selected.
-
 <template>
-  <v-card
-    class="mx-auto"
-    max-width="400"
-  >
-    <v-list>
-      <v-list-item-group
-        v-model="model"
-        active-class="border"
-        color="indigo"
-      >
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-        >
-          <v-list-item-icon>
-            <v-icon v-text="item.icon"></v-icon>
-          </v-list-item-icon>
-
-          <v-list-item-content>
-            <v-list-item-title v-text="item.text"></v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list-item-group>
-    </v-list>
-  </v-card>
+    <body>
+    <div class='div2'>
+      <button class="open-button" v-on:click="openForm()">Add new user</button>
+        <div class="form-popup" id="myForm">
+          <form class="form-container">
+            <h2>New User of Expired?</h2>
+            <label for="username"><b>Username:</b></label>
+            <input v-model="username" type="text" placeholder="Enter your name/role" ><br>
+            <button type="submit" v-on:click.prevent="addUserFirebase()">Add</button>
+            <button type="button" v-on:click="closeForm()">Back</button>
+          </form>
+        </div>
+      <button type="button" class="btn" v-on:click="select()">Select</button>
+    </div>
+    </body>
 </template>
 
 <script>
-  export default {
-    data: () => ({
-      items: [
-        {
-          icon: 'mdi-wifi',
-          text: 'Wifi',
-        },
-        {
-          icon: 'mdi-bluetooth',
-          text: 'Bluetooth',
-        },
-        {
-          icon: 'mdi-chart-donut',
-          text: 'Data Usage',
-        },
-      ],
-      model: 1,
-    }),
-  }
+import firebase from 'firebase/app';
+export default {
+    name: 'SelectUserPage',
+    data() {
+        return {
+          username:"",
+        };
+    },
+   methods:{
+    openForm() {
+      document.getElementById("myForm").style.display = "block";
+    },
+    closeForm() {
+      document.getElementById("myForm").style.display = "none";
+    },
+    next() {
+      this.$router.push('/users');
+    },
+    addUserFirebase() {
+        const foodRef = firebase.firestore().collection('users')
+        const userid = firebase.auth().currentUser.uid
+        foodRef.doc(userid).collection("family").doc(this.username).set(
+          {
+            username:this.username,
+            createdOn:new Date(),
+          },
+        ).then(()=> this.reload())
+        alert("Document is written successfully")
+        document.getElementById("myForm").style.display = "none";
+        this.$router.push('/users');
+        this.username=''
+      },
+    select() {
+      this.$router.push('/home');
+    }
+  },
+};
 </script>
 
 <style scoped>
-.border {
-  border: 2px dashed orange;
-}
+   body { 
+        font-family: 'Ubuntu', sans-serif;
+        float: center;
+        padding: 260px;
+        background-image: url('https://images.theconversation.com/files/282104/original/file-20190701-105182-1q7a7ji.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=900.0&fit=crop');
+        background-repeat: no-repeat;
+        background-size: 100% 100%;
+    }
+    button {
+      cursor: pointer;
+        border-radius: 5em;
+        color: #fff;
+        float: center;
+        background: linear-gradient(to right, #9C27B0, #E040FB);
+        border: 0;
+        padding-left: 40px;
+        padding-right: 40px;
+        padding-bottom: 10px;
+        padding-top: 10px;
+        font-family: 'Ubuntu', sans-serif;
+        font-size: 13px;
+        box-shadow: 0 0 20px 1px rgba(0, 0, 0, 0.04);
+    }
+    .div2 {
+        padding-top: 5px;
+        padding-bottom: 30px;
+        background-color: floralwhite;
+    }
+    input {
+        padding : 10px;
+        border-radius: 5em;
+        border-color: rgb(197, 141, 141);
+    }
+    .form-popup {
+      display: none;
+    }
 </style>
