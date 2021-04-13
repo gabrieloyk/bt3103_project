4fe0ea346dc58b4fa97bfe486b578748e47b4ad2
junkai30 <65784250+junkai30@users.noreply.github.com>
2021-04-14 04:34:11 +0800

    updated family page with profile cards

--- project/src/components/Family.vue
+++ project/src/components/Family.vue
@@ -2,6 +2,20 @@
     <div>
         <app-header></app-header>
         <p>This is family page</p>
+        <div class="wrapper">
+          <div class="card" v-for="profile in profiles" v-bind:key="profile.username" v-on:click="profile.show = !profile.show"
+          :class="{active:profile.username === selected}">
+            <a>
+            <img v-bind:src="profile.imgfile"/>
+            <br>
+            <b>{{profile.username}}</b>
+            </a>
+            <div v-show="profile.show">
+              <button v-on:click="selected = profile.username" class="btn">Select</button>
+              <button class="btn">Delete</button>
+            </div>
+          </div>
+        </div>
     </div>
 </template>
 
@@ -9,17 +23,36 @@
 //Register Locally
 
 import Header from '../components/Header.vue'
+import firebase from 'firebase/app'
 //import Footer from './components/Footer.vue'
 
 export default {
  data(){
     return{
-      
+      profiles: [],
+      selected: null
     }
   },
    methods:{
-     
+     fetchItems() {
+       const userid = firebase.auth().currentUser.uid
+       firebase.firestore().collection('users').doc(userid).collection('family').onSnapshot((querySnapShot) => {
+         this.profiles = [];
+         let profile = {}
+         querySnapShot.docs.forEach((doc) =>{
+           profile = doc.data()
+           profile.show = false
+           this.profiles.push(profile)
+         })
+       })
+     },
+
    },
+
+   created() {
+     this.fetchItems()
+   },
+
    //Register Locally
   components:{
     'app-header':Header,
@@ -31,11 +64,72 @@ export default {
 </script>
 
 <style scoped>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;  
-  text-align: center;
-  color: #4c2792be;
-  font-size:14px;
+html, body {
+  display: -moz-grid-line;
+  align-items: center;
+  justify-content: center;
+  /*flex-direction: column;*/
+  margin-top: 16px;
+  margin-bottom: 16px;
+}
+
+.wrapper {
+  display: flex;
+  max-width:100%;
+  flex-wrap:wrap;
+  padding-top:12px;
+  align-items:center;
+  justify-content:center;
+}
+
+@keyframes animated-border {
+  0% {
+    box-shadow: 0 0 0 0 rgba(0,0,0,0.4);
+  }
+  100% {
+     box-shadow: 0 0 0 20px rgba(0,0,0,0);
+  }
+}
+
+.card {
+    box-shadow: rgba(0, 0, 0, 0.117647) 0px 1px 6px, rgba(0, 0, 0, 0.117647) 0px 1px 4px;
+    max-width: 100%;
+    margin: 12px;
+    transition: .15s all ease-in-out;
+    padding: 10px;
+    border-radius:40%;
+    border: 4px solid;
+    font-family: Impact, fantasy;
+    color: #b294eb;
+    
+}
+
+.active {
+    animation: animated-border 1.5s infinite;
+
+}
+
+img {
+  height:100px;
+  width:150px;
+  border-radius:40px;
+}
+
+a {
+    text-decoration: none;
+      padding: 12px;
+      color: black;
+      font-size: 24px;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+}
+
+.btn {
+  font-family: Impact, fantasy;
+  background-color:#b294eb;
+  color:white;
+  border-radius:10px;
 }
 
 </style>
