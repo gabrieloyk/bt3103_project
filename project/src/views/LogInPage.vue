1e2647631a208ce1496a5ec514a0494ee513e733
melissateozuki <melissateozuki@gmail.com>
2021-04-18 22:35:06 +0800

    Edit log in page look

--- project/src/views/LogInPage.vue
+++ project/src/views/LogInPage.vue
@@ -1,26 +1,28 @@
 <template>
     <body>
     <div class='div1'>
-        <router-link to="/" style="color:white">Log In</router-link> |
-        <router-link to="/register" style="color:white">Register</router-link> 
-
-        <form @submit.prevent="login" id="form1">
-            <h1>Welcome to "Expired?"!</h1>
-            <h2>Login</h2>
-            <input
-                type="email"
-                placeholder="Email address..."
-                v-model="email"
-            /><br><br>
-            <input
-                type="password"
-                placeholder="password..."
-                v-model="password"
-            />
-            <p v-on:click="forgot()" style="font-style:italic;color:#bd9bda"> Forgot Password?</p>
-            <button type="submit">Login</button>
-            <br><br>
-        </form>
+        <img src="https://images.theconversation.com/files/282104/original/file-20190701-105182-1q7a7ji.jpg" >
+        <div id="content">
+            <router-link to="/" style="color:white">Log In</router-link> |
+            <router-link to="/register" style="color:white">Register</router-link> 
+            <form @submit.prevent="login" id="form1">
+                <h1>Welcome to "Expired?"!</h1>
+                <h2>Login</h2>
+                <input
+                    type="email"
+                    placeholder="Email address..."
+                    v-model="email"
+                /><br><br>
+                <input
+                    type="password"
+                    placeholder="password..."
+                    v-model="password"
+                />
+                <p v-on:click="forgot()" style="font-style:italic;color:#bd9bda"> Forgot Password?</p>
+                <button type="submit">Login</button>
+                <br><br>
+            </form>
+        </div>
     </div>
     </body>
 </template>
@@ -62,14 +64,6 @@ export default {
 </script>
 
 <style scoped>
-   body { 
-        font-family: 'Ubuntu', sans-serif;
-        float: center;
-        padding: 260px;
-        background-image: url('https://images.theconversation.com/files/282104/original/file-20190701-105182-1q7a7ji.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=900.0&fit=crop');
-        background-repeat: no-repeat;
-        background-size: 100% 100%;
-    }
     button {
       cursor: pointer;
         border-radius: 5em;
@@ -90,6 +84,12 @@ export default {
         padding-bottom: 30px;
         background-color: floralwhite;
     }
+    #content{
+        margin-top: 10%;
+        float:right;
+        width:50%;
+        background-color:#bd9bda;
+    }
     input {
         padding : 10px;
         border-radius: 5em;
@@ -97,5 +97,20 @@ export default {
     }
     h1 {
         color: #9C27B0;
+    }
+    .div1 {
+        background-color:#bd9bda;
+        height:100vh;
+        padding: 0;
+        margin: 0;
+        width: 100%;
+        min-height: 100%;
+    }
+    router-link {
+        float:right;
+    }
+    img {
+        width:50%;
+        height:100%;
     }
 </style>
